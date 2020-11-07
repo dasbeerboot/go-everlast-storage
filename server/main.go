9211cf397d5970cc97c5b9b7e0d71c08118e8251
Youngjoon Lee <yjlee@medibloc.org>
2020-11-07 23:00:52 +0900

    Add config to get environment variables

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"context"
+	"fmt"
 	"log"
 	"net"
 	"os"
@@ -11,24 +12,26 @@ import (
 	"google.golang.org/grpc"
 )
 
-type server struct {
+type fileServer struct {
 	pb.UnimplementedFileManagerServer
+
+	conf *config
 }
 
-func (s *server) PutFile(ctx context.Context, in *pb.PutFileRequest) (*pb.PutFileResponse, error) {
+func (s *fileServer) PutFile(ctx context.Context, in *pb.PutFileRequest) (*pb.PutFileResponse, error) {
 	var fileName = in.GetFileName()
 	var fileContext = in.GetFileContext()
 
 	log.Printf("Received fileName and fileContext as : %v", fileName+" | "+fileContext)
 
-	writeFile(fileName, fileContext)
+	writeFile(s.conf.basePath, fileName, fileContext)
 
 	// writeFile 에서 string을 리턴하고, 현재 함수에서 return값을 받아와서 PutFileResponse로 리턴할 수 없으면 결과 어떻게 로그찍음??
 	return &pb.PutFileResponse{Result: "File saved successfully"}, nil
 }
 
-func writeFile(fileName string, fileContext string) {
-	file, err := os.Create("/Users/juwon/Desktop/go-storage/" + fileName + ".txt")
+func writeFile(basePath string, fileName string, fileContext string) {
+	file, err := os.Create(fmt.Sprintf("%s/%s.txt",  basePath, fileName))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,11 +45,11 @@ func writeFile(fileName string, fileContext string) {
 	log.Printf("File saved successfully")
 }
 
-func (s *server) GetFile(ctx context.Context, in *pb.GetFileRequest) (*pb.GetFileResponse, error) {
+func (s *fileServer) GetFile(ctx context.Context, in *pb.GetFileRequest) (*pb.GetFileResponse, error) {
 	var fileName = in.GetFileName()
 	log.Printf("Received fileName : " + fileName)
 
-	data, err := os.Open("/Users/juwon/Desktop/go-storage/" + fileName + ".txt")
+	data, err := os.Open(fmt.Sprintf("%s/%s.txt", s.conf.basePath, fileName))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -66,14 +69,21 @@ func (s *server) GetFile(ctx context.Context, in *pb.GetFileRequest) (*pb.GetFil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", ":8088")
+	config, err := newConfig()
+	if err != nil {
+		log.Fatalf("failed to get config: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", config.port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
-	pb.RegisterFileManagerServer(s, &server{})
-	if err := s.Serve(lis); err != nil {
+	fileSvr := &fileServer{conf: config}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterFileManagerServer(grpcServer, fileSvr)
+	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
